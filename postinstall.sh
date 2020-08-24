a642fa0e00261deb6356ba2a9c0cbc23f15c43f8
Vitor Pavan <vipavani@hotmail.com>
2020-08-25 00:19:13 +0200

    first commit

---
+++ postinstall.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+# ----------------------------- VARIÁVEIS ----------------------------- #
+
+URL_WINE_KEY="https://dl.winehq.org/wine-builds/winehq.key"
+URL_PPA_WINE="https://dl.winehq.org/wine-builds/ubuntu/"
+URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+
+DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
+
+PROGRAMAS_PARA_INSTALAR=(
+  snapd
+  virtualbox
+)
+# ---------------------------------------------------------------------- #
+
+# ----------------------------- REQUISITOS ----------------------------- #
+## Removendo travas eventuais do apt ##
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/cache/apt/archives/lock
+
+## Atualizando o repositório ##
+sudo apt update -y
+
+## Adicionando repositórios de terceiros e suporte a Snap (Driver Logitech, Lutris e Drivers Nvidia)##
+wget -nc "$URL_WINE_KEY"
+sudo apt-key add winehq.key
+# ---------------------------------------------------------------------- #
+
+# ----------------------------- EXECUÇÃO ----------------------------- #
+## Atualizando o repositório depois da adição de novos repositórios ##
+sudo apt update -y
+
+## Download e instalaçao de programas externos ##
+mkdir "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
+
+## Instalando pacotes .deb baixados na sessão anterior ##
+sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
+
+# Instalar programas no apt
+for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
+  if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
+    apt install "$nome_do_programa" -y
+  else
+    echo "[INSTALADO] - $nome_do_programa"
+  fi
+done
+
+sudo apt install --install-recommends winehq-stable wine-stable wine-stable-i386 wine-stable-amd64 -y
+
+## Instalando pacotes Flatpak ##
+flatpak install flathub com.obsproject.Studio -y
+
+## Instalando pacotes Snap ##
+sudo snap install spotify
+sudo snap install slack --classic
+sudo snap install skype --classic
+sudo snap install photogimp
+# ---------------------------------------------------------------------- #
+
+# ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
+## Finalização, atualização e limpeza##
+sudo apt update && sudo apt dist-upgrade -y
+flatpak update
+sudo apt autoclean
+sudo apt autoremove -y
+# ---------------------------------------------------------------------- #
